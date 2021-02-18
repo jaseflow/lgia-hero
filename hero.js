9d49b644a7f094c43e025f50e1c5819a5827d241
Jason Corbett <jsncbt@gmail.com>
2021-02-19 04:29:55 +1000

    Add all the things

---
+++ hero.js
@@ -0,0 +1,32 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+
+  const hero = document.getElementById('hero')
+  const heroSlides = document.getElementById('hero-slides')
+  const dots = document.querySelectorAll('.hero-dot')
+  const slides = document.querySelectorAll('.hero__content')
+
+  let activeIndex = 0;
+  let manual = false
+
+  dots.forEach((dot, i) => {
+    dot.addEventListener('click', () => {
+      manual = true
+      activeIndex = i
+      hero.setAttribute('class',`hero hero--${activeIndex}`)
+    })
+  })
+
+  hero.setAttribute('class',`hero hero--${activeIndex}`)
+  heroSlides.setAttribute('class', `hero__slides hero__slides--${activeIndex}`)
+
+  setInterval(() => {
+    if (!manual && activeIndex >= (slides.length - 1)) {
+      activeIndex = 0
+    } else if (!manual) {
+      activeIndex ++
+    }
+    hero.setAttribute('class',`hero hero--${activeIndex}`)
+    heroSlides.setAttribute('class',`hero__slides hero__slides--${activeIndex}`)
+  }, 5000)
+
+});
